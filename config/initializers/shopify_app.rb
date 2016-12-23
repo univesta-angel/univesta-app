9487ed564227c07d43f6f34876c6bbb75a50205b
Angelica F. Santiago <angelica.ucic@gmail.com>
2016-12-23 09:31:44 +0800

    generate shopify app

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,5 +3,6 @@ ShopifyApp.configure do |config|
   config.api_key = "<api_key>"
   config.secret = "<secret>"
   config.scope = "read_orders, read_products"
+  config.redirect_uri = "http://shopifyapp-sample.herokuapp.com/auth/shopify/callback"
   config.embedded_app = true
 end
