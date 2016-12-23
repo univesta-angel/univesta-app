ecb7aaebfce22a11f5e453761f70e7e53ff49e17
Angelica F. Santiago <angelica.ucic@gmail.com>
2016-12-23 10:19:18 +0800

    generate shopify app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @products = ShopifyAPI::Product.search(params[:search])
+    @posts = Product.all
+  	if params[:search]
+    	@posts = Product.search(params[:search]).order("created_at DESC")
+  	else
+    	@posts = Product.all.order('created_at DESC')
+  	end
   end
 end
