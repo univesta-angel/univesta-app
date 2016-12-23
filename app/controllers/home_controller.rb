0154f1c36d7ac1a0aa25132db302641ff540012f
Angelica F. Santiago <angelica.ucic@gmail.com>
2016-12-23 10:25:04 +0800

    generate shopify app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,4 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @products = Product.all
-  	if params[:search]
-    	@products = Product.search(params[:search]).order("created_at DESC")
-  	else
-    	@products = Product.all.order('created_at DESC')
-  	end
-  end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,12 +1,4 @@
 class Shop < ActiveRecord::Base
   include ShopifyApp::Shop
   include ShopifyApp::SessionStorage
-
-  def self.search(search)
-	  if search
-	    find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
-	  else
-	    find(:all)
-	  end
-  end
 end
