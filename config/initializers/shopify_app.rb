fdeae5d6093506b39e41f6a10ce68625c644e92b
Angelica F. Santiago <angelica.ucic@gmail.com>
2016-12-23 10:26:19 +0800

    update redirect_uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,7 @@ ShopifyApp.configure do |config|
 
   config.api_key = "345caeafea49d80e0f40194dd9a721c5"
   config.secret = "43f67e64209c57cb6ba9116a9a79be9d"
+  config.redirect_uri = "http://shopifyapp-sample.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
