803575cbbb79e3cdb472d6264bfc1b0b5b0777b6
Angelica F. Santiago <angelica.ucic@gmail.com>
2016-12-29 11:56:36 +0800

    generate shopify app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,18 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
+  	
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+    @import = Import.new(import_params)
+
+    respond_to do |format|
+      if @import.save
+        format.html { redirect_to @import, notice: 'Import was successfully created.' }
+        format.json { render :show, status: :created, location: @import }
+      else
+        format.html { render :new }
+        format.json { render json: @import.errors, status: :unprocessable_entity }
+      end
+    end
+
   end
 end
