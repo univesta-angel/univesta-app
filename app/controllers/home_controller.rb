6b3df783de6337fb32ed546b3fba60844e4cdac7
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-01-12 11:19:49 +0800

    generate shopify app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
 
-    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-
+    #@products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+    @products = ShopifyAPI::Product.find(:all, params: {collection_id: 401443728})
+    
   end
 
   def create
