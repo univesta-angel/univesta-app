366583332592eac7022cb7488c3492075700d4b9
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-01-18 13:05:23 +0800

    generate shopify app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :allow_ajax_request_from_other_domains
+
+  def allow_ajax_request_from_other_domains
+   headers['Access-Control-Allow-Origin'] = '*'
+   headers['Access-Control-Request-Method'] = '*'
+   headers['Access-Control-Allow-Headers'] = '*'
+ end
+ 
 end
