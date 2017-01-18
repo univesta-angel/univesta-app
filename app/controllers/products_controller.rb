181a9a0eef94a03bdc3ccef877901e4d9c9bfa84
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-01-18 10:39:14 +0800

    generate shopify app

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,7 +24,7 @@ class ProductsController < ApplicationController
   # POST /products
   # POST /products.json
   def create
-    @product = Product.new(products_params)
+    @product = Product.new(product_params)
 
     respond_to do |format|
       if @product.save
