988352a622c652505636c44384c8e2bc62abc81b
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-01-18 10:27:52 +0800

    generate shopify app

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :products
   root :to => 'products#index'
   match 'create' => 'products#create', via: :post
-  post 'products#push'
+  #post 'products#push'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
