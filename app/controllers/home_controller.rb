f97446a0ab8f642cf97b63386579f590da97cf55
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-01-18 10:05:43 +0800

    gennerate shopify app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,25 +9,4 @@ class HomeController < ShopifyApp::AuthenticatedController
     
   end
 
-  def create
-  	@product = ShopifyAPI::Product.create(
-		{	
-		  "product": {
-		    "title": "New Dress",
-		    "vendor": "ABC Shop",
-            "product_type": "Dress",
-            "tags": "Import"
-		  }
-		})
-
-  	respond_to do |format|
-      if @product.save
-        format.json { render json: 201 }
-      else
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
-    end
-
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :products
   root :to => 'products#index'
-  match 'create' => 'home#create', via: :post
+  match 'create' => 'products#create', via: :post
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
