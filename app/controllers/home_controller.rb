3567d62501ea16f54db2254cc3cd3fa6491c5d17
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-01-19 16:54:22 +0800

    generate shopify app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,6 @@ class HomeController < ShopifyApp::AuthenticatedController
   wrap_parameters format: [:json]
 
   def index
-  	shop_url = "https://#{API_KEY}:#{PASSWORD}@SHOP_NAME.myshopify.com/admin"
-	ShopifyAPI::Base.site = shop_url
     #@products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @products = ShopifyAPI::Product.find(:all, params: {collection_id: 401443728})
     
