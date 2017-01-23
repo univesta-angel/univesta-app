ba529dd3f01ae2733c00b6ce4614a2bc4535df73
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-01-23 09:04:33 +0800

    generate shopify app

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,9 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
+    shop_url = "https://2d69dfd97a185d97d49cb4b85de5e76f:1cd78cc392fe8861b891a3f881b3c5d8@gels-store.myshopify.com/admin"
+    ShopifyAPI::Base.site = shop_url
+    shop = ShopifyAPI::Shop.current
     @products = Product.all
   end
 
@@ -40,8 +43,6 @@ class ProductsController < ApplicationController
 
   # push to store action
   def push
-    shop_url = "https://4d60f4fb9986af453d74b9bee9369fd1:000a1b69647c053726bbdd82b103a83d@gels-store.myshopify.com/admin"
-    ShopifyAPI::Base.site = shop_url
     #@product = Product.find(product_params)
     #@product = ShopifyAPI::Product.create(product_params)
     new_product = ShopifyAPI::Product.new
