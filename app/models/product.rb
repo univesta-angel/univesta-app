90d1f02d0559d0c8e88df480b80875198479002c
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-01-26 16:46:51 +0800

    commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
 	def self.search(search)
 	  if search
-	    where('title LIKE ?', "%#{search}%")
+	    where('lower(title) LIKE ?', "%#{search.downcase}%")
 	  else
 	    all
 	  end
