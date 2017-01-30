1957fb88e0575a0f251d3b7a8914ef5bc8d288f5
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-01-30 10:00:53 +0800

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123053702) do
+ActiveRecord::Schema.define(version: 20170127021731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "products", force: :cascade do |t|
+  create_table "imports", force: :cascade do |t|
     t.string   "title"
     t.text     "body_html"
     t.binary   "images"
