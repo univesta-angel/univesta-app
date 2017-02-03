e650991268206728b1141b90fc008d6188465998
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-02-03 10:27:29 +0800

    commit

--- db/migrate/20170203022627_addanothercolumn.rb
+++ db/migrate/20170203022627_addanothercolumn.rb
@@ -1,4 +1,4 @@
-class AddColumnstoProducts < ActiveRecord::Migration[5.0]
+class Addanothercolumn < ActiveRecord::Migration[5.0]
   def change
   	add_column :products, :colors, :string, array: true
   	add_column :products, :sizes, :string, array: true
