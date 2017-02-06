330e0b0bd70708c03da67c3275b0aa005eb06d83
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-02-06 09:58:06 +0800

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -39,6 +39,7 @@ class ProductsController < ApplicationController
                               compare_at_price: params[:compare_at_price],
                               colors: params[:colors],
                               sizes: params[:sizes],
+                              avail_qty: params[:avail_qty],
                               variant_images: params[:variant_images])
 
     respond_to do |format|
@@ -73,6 +74,7 @@ class ProductsController < ApplicationController
     color = params[:_colors].split
     size = params[:_sizes].split
     prices = params[:_prices].split
+    qty = params[:_avail_qty].split
 
     if color.size==0
       color=['-']
@@ -82,6 +84,7 @@ class ProductsController < ApplicationController
     end
     
     i = 0;
+    x = 0;
     variants = []
     color.each do |row1|
       size.each do |row2|
@@ -92,9 +95,10 @@ class ProductsController < ApplicationController
           :compare_at_price     => params[:_compare_at_price],
           :sku                  => params[:_sku],
           :inventory_management => 'shopify',
-          :inventory_quantity   => 10,
+          :inventory_quantity   => params[x],
         )
         variants << zzz
+        x = x+1;
         i = i+1;
       end
     end
