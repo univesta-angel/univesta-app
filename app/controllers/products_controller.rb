385e198ca1907e320c3a83d79e91291f405b51d7
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-02-07 09:30:42 +0800

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -122,7 +122,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if new_product.save
-        format.json { head :ok }
+        format.json { head 200 }
       else
         format.html { redirect_to root_path, notice: 'Oops. Something went wrong.' }
         format.json { render json: new_product.errors, status: :unprocessable_entity }
