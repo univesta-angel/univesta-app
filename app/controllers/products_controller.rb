0fb7312a8ba63a505b38cf0d101019089eb0b9bc
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-02-07 15:44:05 +0800

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -119,15 +119,8 @@ class ProductsController < ApplicationController
     
     expires_in(60.seconds, public: false)
 
-    respond_to do |format|
-      if new_product.save
-        format.html { redirect_to root_path, notice: 'Product was successfully pushed.' }
-        format.json { render json: new_product, status: 201, location: new_product }
-      else
-        format.html { redirect_to root_path, notice: 'Oops. Something went wrong.' }
-        format.json { render json: new_product.errors, status: :unprocessable_entity }
-      end
-    end
+    head 201
+    respond_to :html, :json
   end
 
   # PATCH/PUT /products/1
