69fef4b2f19597eb31000b3a0bd63ba01b487a72
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-02-07 11:26:16 +0800

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -121,7 +121,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if new_product.save
-        format.html { redirect_back fallback_location: '/admin/products.json', notice: 'Product was successfully pushed.', status: 301 }
+        format.html { layout: !request.xhr? }
         format.json { head 201 }
       else
         format.html { redirect_to root_path, notice: 'Oops. Something went wrong.' }
