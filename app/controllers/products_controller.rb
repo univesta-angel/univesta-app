aabdc46389b00997e7245a19fa246a7cae248b2e
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-02-07 13:36:52 +0800

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -121,7 +121,15 @@ class ProductsController < ApplicationController
     
     expires_in(60.seconds, public: false)
 
-    head 201
+    respond_to do |format|
+      if new_product.save
+        format.html { redirect_to :action=>'push', :status => :moved_permanently }
+        format.json { head 201 }                  
+      else
+        format.html { redirect_to root_path, notice: 'Oops. Something went wrong.' }
+        format.json { render json: new_product.errors, status: :unprocessable_entity }
+      end
+    end
 
 
 
