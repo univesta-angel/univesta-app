9ced3659411ddef357a8785de2aacfb8e63802eb
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-02-07 09:32:46 +0800

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -122,6 +122,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if new_product.save
+        format.html { redirect_to root_path, notice: 'Product was successfully pushed.' }
         format.json { head 200 }
       else
         format.html { redirect_to root_path, notice: 'Oops. Something went wrong.' }
