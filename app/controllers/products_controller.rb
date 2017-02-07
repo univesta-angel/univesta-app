0ce674c4da205f49e1c47f307d0e9dc1c4aeabba
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-02-07 10:01:28 +0800

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -107,15 +107,16 @@ class ProductsController < ApplicationController
       end
     end
 
-    new_product = ShopifyAPI::Product.new
-    new_product.title = params[:_title]
-    new_product.body_html = params[:_body]
-    new_product.product_type = params[:_type]
-    new_product.vendor = params[:_vendor]
-    new_product.images = images
-    new_product.tags = params[:_tags]
-    new_product.options = options
-    new_product.variants = variants
+    new_product = ShopifyAPI::Product.new(
+      :title => params[:_title]
+      :body_html => params[:_body]
+      :product_type => params[:_type]
+      :vendor => params[:_vendor]
+      :images => images
+      :tags => params[:_tags]
+      :options => options
+      :variants => variants
+    )
     new_product.save
     
     expires_in(60.seconds, public: false)
@@ -151,7 +152,7 @@ class ProductsController < ApplicationController
     @product.destroy
     respond_to do |format|
       format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
-      format.json { head :created }
+      format.json { head :no_content }
     end
   end
 
