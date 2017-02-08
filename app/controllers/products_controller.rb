092df2db1510b7ba6e50528723fef5cffd8f0e20
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-02-08 09:20:15 +0800

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -55,7 +55,7 @@ class ProductsController < ApplicationController
 
   # push to store action
   def push
-    shop_url = "https://2d69dfd97a185d97d49cb4b85de5e76f:1cd78cc392fe8861b891a3f881b3c5d8@gels-store.myshopify.com/admin/products.json"
+    shop_url = "https://2d69dfd97a185d97d49cb4b85de5e76f:1cd78cc392fe8861b891a3f881b3c5d8@gels-store.myshopify.com/admin"
     ShopifyAPI::Base.site = shop_url
     shop = ShopifyAPI::Shop.current
 
