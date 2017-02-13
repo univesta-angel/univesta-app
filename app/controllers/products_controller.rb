65e423ed0a02e48a60cc1b6acde252e226e08886
univesta-angel <angelica.ucic@gmail.com>
2017-02-13 13:40:32 +0800

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -130,7 +130,7 @@ class ProductsController < ApplicationController
           color2 << new_product.variants[ctr].id
           ctr = ctr + 1
         end
-        pao << { id: nil, variant_ids: color2, src: variant_img[aaa] }
+        pao << { id: nil, variant_ids: color2, src: default_img[aaa] }
         aaa = aaa + 1
       end
     else
