6f7c14f9be41e4344938a4dd834a426080a1d161
univesta-angel <angelica.ucic@gmail.com>
2017-02-13 13:54:04 +0800

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -121,16 +121,16 @@ class ProductsController < ApplicationController
     ctr = 0 
 
     if color.size==0
-      color2=[]
-      size.each do |row2|  
-        color2 << new_product.variants[ctr].id
-        ctr = ctr + 1
-      end
-      pao << { id: nil, variant_ids: color2[0], src: default_img[0] }
+        color2=[]
+        size.each do |row2|  
+          color2 << new_product.variants[ctr].id
+          ctr = ctr + 1
+        end
+        pao << { id: nil, variant_ids: color2, src: default_img[0] }
     else
       color.each do |row1|
         color2=[]
-        size.each do |row2|  
+        size.each do |row2|
           color2 << new_product.variants[ctr].id
           ctr = ctr + 1
         end
