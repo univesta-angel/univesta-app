9e668aa5f927ac7c6982dabd73228e4f23bc41b0
univesta-angel <angelica.ucic@gmail.com>
2017-02-14 16:46:13 +0800

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,27 +12,6 @@
 //
 //= require jquery
 //= require bootstrap-sprockets
-//= require bootstrap-datepicker
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-var todaysDate = new Date();
-
-$(document).on("focus", "[data-behaviour~='datepicker']", function(e){
-    $(this).datepicker({"format": "yyyy-mm-dd", "weekStart": 1, "autoclose": true})
-    $(this).datepicker().on('changeDate', function(ev) {
-    	{alert ('Changed')};
-
-    })
-    // $(this).datepicker.on('changeDate', functin(ev) {
-    // 	// if (ev.date.valueOf() > todaysDate.valueOf()) {
-    // 	// 	$('#alert').show().find('strong').text('Date of Birth must not be after today!');
-    // 	// } else {
-    //  //        $('#alert').hide();
-    //  //        startDate = new Date(ev.date);
-    //  //        $('#date-start-display').text($('#date-start').data('date'));    		
-    // 	// }
-    //     // $('#date_dob').datepicker('hide');    	
-    // }) ;
-});
