db60ee469dfa44adbe11666761ddf4521cec3f64
univesta-angel <angelica.ucic@gmail.com>
2017-02-20 09:54:46 +0800

    x

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,5 +1,8 @@
-class OrderController < ShopifyApp::AuthenticatedController
+class OrderController < ApplicationController
   def index
+      shop_url = "https://2d69dfd97a185d97d49cb4b85de5e76f:1cd78cc392fe8861b891a3f881b3c5d8@gels-store.myshopify.com/admin"
+      ShopifyAPI::Base.site = shop_url
+      shop = ShopifyAPI::Shop.current
     
       @orders = ShopifyAPI::Order.find(:all)
 
