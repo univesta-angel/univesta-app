f1bf6e8a94a9672823c0cd4d411a2fc0165be1a4
univesta-angel <angelica.ucic@gmail.com>
2017-02-20 10:25:41 +0800

    x

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -3,6 +3,12 @@ class OrderController < ApplicationController
     shop_url = "https://2d69dfd97a185d97d49cb4b85de5e76f:1cd78cc392fe8861b891a3f881b3c5d8@gels-store.myshopify.com/admin"
     ShopifyAPI::Base.site = shop_url
     shop = ShopifyAPI::Shop.current
-    @orders = ShopifyAPI::Order.find(:all)
+    
+    count = ShopifyAPI::Order.find(:count)
+    pages = count % 250
+    1.upto(pages) do |page|
+      @orders = ShopifyAPI::Order.find(:all, params: {limit: 250, page: page})
+    end
+    
   end
 end
