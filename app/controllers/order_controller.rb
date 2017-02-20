a329b8653f6f6637a2d5ef6ae9fd0c45f4fdaddd
univesta-angel <angelica.ucic@gmail.com>
2017-02-20 09:45:27 +0800

    x

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,13 +1,7 @@
 class OrderController < ApplicationController
   def index
     
-    
-    count = ShopifyAPI::Order.find(:count)
-    pages = count % 250
-    order = nil
-    1.upto(pages) do |page|
-      @orders = ShopifyAPI::Order.find(:all, params: {limit: 250, page: page})
-    end
-    
+      @orders = ShopifyAPI::Order.find(:all)
+
   end
 end
