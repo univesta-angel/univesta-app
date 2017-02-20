69c6ee8f7961294dbcf554fb119a115f3e58be51
univesta-angel <angelica.ucic@gmail.com>
2017-02-20 10:47:31 +0800

    x

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -4,17 +4,7 @@ class OrderController < ApplicationController
     ShopifyAPI::Base.site = shop_url
     shop = ShopifyAPI::Shop.current
     
-    count = GET /admin/orders/count.json
-    page_size = 250
-
-    pages = ceiling(count / 250.0)
-
-    current_page = 1
-    orders = []
-
-    while(current_page <= pages)
-      orders += GET /admin/orders.json?limit=250&page=current_page
-      current_page += 1
+    @orders = ShopifyAPI::Order.find(:all)
     
   end
 end
