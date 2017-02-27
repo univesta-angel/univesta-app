fed556e3b575e674b1f7a316570832b14a174ee5
univesta-angel <angelica.ucic@gmail.com>
2017-02-27 08:17:31 +0800

    x

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,19 @@ Rails.application.routes.draw do
   end
   
   root :to => 'products#index'
+  
+  get 'myproducts' => 'my_products#index'
+  
   match 'create' => 'products#create', via: :post
+  
   post 'push' => 'products#push', as: :push
   post 'push_all' => 'products#push', as: :push_all
-  get 'myproducts' => 'my_products#index'
-  get 'orderlist' => 'products#order', as: :orderlist
+  
+  post 'edit_price' => 'imports#edit_price', as: :edit_price
+  post 'edit_cap' => 'imports#edit_cap', as: :edit_cap
+  delete 'remove_all' => 'imports#remove_all', as: :remove_all
+  delete 'imports_remove' => 'imports#remove_selected', as: :remove_selected
+  
 
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
