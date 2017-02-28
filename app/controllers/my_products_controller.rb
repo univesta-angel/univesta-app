327cde94b398062283db9b49b339122044874d5b
univesta-angel <angelica.ucic@gmail.com>
2017-02-28 15:52:14 +0800

    x

--- app/controllers/my_products_controller.rb
+++ app/controllers/my_products_controller.rb
@@ -1,5 +1,6 @@
 class MyProductsController < ApplicationController
   def index
+    ShopifyAPI::Base.activate_session(session[:shopify])
     @myproducts = ShopifyAPI::Product.find(:all)
   end
 end
