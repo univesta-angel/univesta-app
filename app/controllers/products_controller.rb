a946304cac6e88ea80a5b1aeac2ec08d3fb3cfda
univesta-angel <angelica.ucic@gmail.com>
2017-03-06 08:36:54 +0800

    x

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,9 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     #@products = Product.all
+    shop_url = "https://2d69dfd97a185d97d49cb4b85de5e76f:1cd78cc392fe8861b891a3f881b3c5d8@gels-store.myshopify.com/admin"
+    ShopifyAPI::Base.site = shop_url
+    shop = ShopifyAPI::Shop.current
 
     @products = Product.search(params[:search])
     #@collects = ShopifyAPI::Collect.find(:all)    #get collection
