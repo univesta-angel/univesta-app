e094f206a8fedd74a14c7ff5b5478f3479ea79b9
univesta-angel <angelica.ucic@gmail.com>
2017-03-14 10:14:40 +0800

    Update my_products_controller.rb

--- app/controllers/my_products_controller.rb
+++ app/controllers/my_products_controller.rb
@@ -1,6 +1,5 @@
 class MyProductsController < ApplicationController
   def index
-    ShopifyAPI::Base.activate_session(session[:shopify])
     @myproducts = ShopifyAPI::Product.find(:all)
   end
 end
