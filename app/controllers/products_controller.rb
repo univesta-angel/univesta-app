5eefc61fd95cd8352b6221e3b3b5d77d8067c780
univesta-angel <angelica.ucic@gmail.com>
2017-03-16 08:58:47 +0800

    z

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -205,6 +205,13 @@ class ProductsController < ApplicationController
       variants << zzz
     end
     
+    ae_url = ''
+    mf_key = ''
+    Product.find(params[:pid]) do |p|
+      ae_url = p.ae_url
+      mf_key = p.mf_key
+    end
+    
     new_product = ShopifyAPI::Product.new
     new_product.title = params[:_title]
     new_product.body_html = params[:content].gsub('&nbsp;', '').gsub('/','\/')
@@ -213,6 +220,7 @@ class ProductsController < ApplicationController
     new_product.tags = params[:_tags]
     new_product.options = options
     new_product.variants = variants
+    new_product.metafields = ShopifyAPI::Metafield.new(:key => mf_key,:value => ae_url,:value_type => 'string',:namespace => mf_key)
     new_product.save
     
     pao = []
@@ -254,21 +262,13 @@ class ProductsController < ApplicationController
     end
     
     new_product.images = pao
-    
-    ae_url = ''
-    mf_key = ''
-    Product.find(params[:pid]) do |p|
-      ae_url = p.ae_url
-      mf_key = p.mf_key
-    end
-    
-    new_product.metafields = ShopifyAPI::Metafield.new(:key => mf_key,:value => ae_url,:value_type => 'string',:namespace => mf_key)
     new_product.save
     
     #if params[:_collections] != nil
       #ShopifyAPI::Collect.create(:product_id => new_product.id, :collection_id => params[:_collections])
     #else
     #end
+    
     
   end
   
