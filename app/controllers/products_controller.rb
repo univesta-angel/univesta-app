3bd9a7d1e78e4a0be060c3d0a76ffd49a8186704
univesta-angel <angelica.ucic@gmail.com>
2017-03-16 09:49:27 +0800

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -205,13 +205,6 @@ class ProductsController < ApplicationController
       variants << zzz
     end
     
-    ae_url = ''
-    mf_key = ''
-    Product.find(params[:pid]) do |p|
-      ae_url = p.ae_url
-      mf_key = p.mf_key
-    end
-    
     new_product = ShopifyAPI::Product.new
     new_product.title = params[:_title]
     new_product.body_html = params[:content].gsub('&nbsp;', '').gsub('/','\/')
@@ -262,6 +255,12 @@ class ProductsController < ApplicationController
     new_product.images = pao
     new_product.save
     
+    ae_url = ''
+    mf_key = ''
+    Product.find(params[:pid]) do |p|
+      ae_url = p.ae_url
+      mf_key = p.mf_key
+    end
     new_product.add_metafield(ShopifyAPI::Metafield.new(:namespace => me_key, :key => mf_key, :value => ae_url, :value_type => "string"))
     #if params[:_collections] != nil
       #ShopifyAPI::Collect.create(:product_id => new_product.id, :collection_id => params[:_collections])
