d5ca28a271066062bcf1d33a943dbf75270b7752
univesta-angel <angelica.ucic@gmail.com>
2017-03-21 09:34:20 +0800

    Update order.js

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -3,9 +3,7 @@ var editorExtensionId = "abcdefghijklmnoabcdefhijklmnoabc";
 
 // Make a simple request:
 document.getElementById("order-btn").addEventListener("click", function() {
-  var go = function() {
         var event = document.createEvent('Event');
         event.initEvent('hello');
         document.dispatchEvent(event);
-    }
 });
