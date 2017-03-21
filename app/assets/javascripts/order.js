f4b9e632f6013f728d120d52856243860a477595
univesta-angel <angelica.ucic@gmail.com>
2017-03-21 08:15:59 +0800

    x

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,9 +1,7 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
 var editorExtensionId = "ohkdopmbjjcdmhcefhdafakjocmdnddp";
 
-$('.order-prd-btn').click(function(){
+$('.order-prd-btn').click(function(event){
+  event.preventDefault();
   chrome.runtime.sendMessage(editorExtensionId, {openUrlInEditor: url},
     function(response) {
       if (!response.success)
