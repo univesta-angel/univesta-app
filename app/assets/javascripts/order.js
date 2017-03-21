f76911d62d719e3d7f1adccfc3e431c4ffd6ae4d
univesta-angel <angelica.ucic@gmail.com>
2017-03-21 09:07:11 +0800

    Update order.js

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,5 +1,9 @@
-document.getElementById("order-btn").addEventListener("click",
-    function() {
-  window.postMessage({ type: "FROM_PAGE_TO_CONTENT_SCRIPT", 
-     text: "Hello from the webpage!" }, "*");
-}, false);
+// The ID of the extension we want to talk to.
+var editorExtensionId = "abcdefghijklmnoabcdefhijklmnoabc";
+
+// Make a simple request:
+chrome.runtime.sendMessage(editorExtensionId, {openUrlInEditor: url},
+  function(response) {
+    if (!response.success)
+      handleError(url);
+  });
