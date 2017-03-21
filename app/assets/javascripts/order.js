60ab6c769a20b3ac7e04d4da67f9974101022ed1
univesta-angel <angelica.ucic@gmail.com>
2017-03-21 09:38:55 +0800

    Update order.js

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,9 +1,1 @@
-// The ID of the extension we want to talk to.
-var editorExtensionId = "abcdefghijklmnoabcdefhijklmnoabc";
 
-// Make a simple request:
-document.getElementById("order-btn").addEventListener("click", function() {
-        var event = document.createEvent('Event');
-        event.initEvent('hello');
-        document.dispatchEvent(event);
-});
