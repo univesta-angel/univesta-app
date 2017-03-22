2537493cea81c7462bc01f01d8916ccc150c7b83
univesta-angel <angelica.ucic@gmail.com>
2017-03-22 13:28:15 +0800

    x

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,5 +1,9 @@
-$("#order-btn").click(function(){
-    var event = document.createEvent('Event');
-    event.initEvent('hello');
-    document.dispatchEvent(event);  
+$(document).ready(function(){
+  $("#order-btn").click(function(){
+      var event = document.createEvent('Event');
+      event.initEvent('hello');
+      document.dispatchEvent(event);
+      var product_link = $("#prd_link").text();
+      window.open(product_link, "_self")
+  });
 });
