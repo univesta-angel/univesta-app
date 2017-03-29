0c5bd58b5ccae53b0e9dbbccf0647f4996321c67
univesta-angel <angelica.ucic@gmail.com>
2017-03-29 16:09:39 +0800

    x

--- app/controllers/my_products_controller.rb
+++ app/controllers/my_products_controller.rb
@@ -2,4 +2,8 @@ class MyProductsController < ApplicationController
   def index
     @myproducts = ShopifyAPI::Product.find(:all)
   end
+  
+  def delete_my_product
+    ShopifyAPI::Product.delete(:prd_id)
+  end
 end
