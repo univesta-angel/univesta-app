791a89a733c2b521aca1c82c44c34029f792386d
univesta-angel <angelica.ucic@gmail.com>
2017-03-30 16:17:52 +0800

    x

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,7 +26,62 @@
 $(document).ready(function () {
   
   //$('.datepicker').datepicker();
-  
+  var start = moment().subtract(29, 'days');
+  var end = moment();
+
+  $('#daterange').click(function() {
+    alert('hi');
+  });
+  $('#daterange').daterangepicker({
+      "ranges": {
+          "Today": [
+              "2017-03-30T01:19:42.901Z",
+              "2017-03-30T01:19:42.901Z"
+          ],
+          "Yesterday": [
+              "2017-03-29T01:19:42.901Z",
+              "2017-03-29T01:19:42.901Z"
+          ],
+          "Last 7 Days": [
+              "2017-03-24T01:19:42.901Z",
+              "2017-03-30T01:19:42.901Z"
+          ],
+          "Last 30 Days": [
+              "2017-03-01T01:19:42.901Z",
+              "2017-03-30T01:19:42.901Z"
+          ],
+          "This Month": [
+              "2017-02-28T16:00:00.000Z",
+              "2017-03-31T15:59:59.999Z"
+          ],
+          "Last Month": [
+              "2017-01-31T16:00:00.000Z",
+              "2017-02-28T15:59:59.999Z"
+          ]
+      },
+      "alwaysShowCalendars": true,
+      "startDate": start,
+      "endDate": end
+  }, function(start, end, label) {
+    //alert(start.format('YYYY-MM-DD'))
+      $.ajax({
+        url: "/orders",
+        type: 'GET',
+        data: {
+          start: start.format('YYYY-MM-DD'),
+          end: end.format('YYYY-MM-DD')
+        },
+        dataType: 'application/json',
+        success: function(data){
+          window.location.reload()
+        }
+      });
+    console.log("A new date range was chosen: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+
+  $('#daterange').on('cancel.daterangepicker', function(ev, picker) {
+    $(this).val('');
+  });   
   var trigger = $('.hamburger'),
       overlay = $('.overlay'),
      isClosed = false;
