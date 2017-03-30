d68386d100dd96190d40be6621c53d2c7b92db15
univesta-angel <angelica.ucic@gmail.com>
2017-03-30 08:15:29 +0800

    x

--- app/controllers/my_products_controller.rb
+++ app/controllers/my_products_controller.rb
@@ -5,8 +5,5 @@ class MyProductsController < ApplicationController
   
   def delete_my_product
     ShopifyAPI::Product.delete(:prd_id)
-    respond_to do |format|
-      format.html { redirect_to my_products_path, notice: 'Product was successfully deleted.' }
-    end
   end
 end
