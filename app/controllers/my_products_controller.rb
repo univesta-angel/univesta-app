b3e6b02e327f824cce70371e085e902e1b53892a
univesta-angel <angelica.ucic@gmail.com>
2017-03-30 08:32:28 +0800

    x

--- app/controllers/my_products_controller.rb
+++ app/controllers/my_products_controller.rb
@@ -1,9 +1,20 @@
 class MyProductsController < ApplicationController
+  before_action :set_my_product, only: [:destroy]
   def index
     @myproducts = ShopifyAPI::Product.find(:all)
   end
   
-  def delete_my_product
-    ShopifyAPI::Product.delete(:prd_id)
+  def destroy
+    @my_product.destroy
+    respond_to do |format|
+      format.html { redirect_to my_products_url, notice: 'My product was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
+  
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_my_product
+      @my_product = ShopifyAPI::Product.find(params[:id])
+    end
 end
