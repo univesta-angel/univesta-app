f064b2331b2b6b3092eefefa8d7a172a83a54190
univesta-angel <angelica.ucic@gmail.com>
2017-03-30 14:48:20 +0800

    x

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -4,7 +4,8 @@ class OrderController < ApplicationController
     ShopifyAPI::Base.site = shop_url
     shop = ShopifyAPI::Shop.current
     
-    @orders = ShopifyAPI::Order.find(:all, :params => {limit: 5, order: "created_at DESC"})
+    #@orders = ShopifyAPI::Order.find(:all, :params => {limit: 5, order: "created_at DESC"})
+    @orders = ShopifyAPI::Order.find(:all, :params => { :created_at_min => params[:start], :created_at_max => params[:end] })
     
   end
 end
