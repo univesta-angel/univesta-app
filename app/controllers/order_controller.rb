51e0ea69b428ade4ca18419334d065bba13d8c81
univesta-angel <angelica.ucic@gmail.com>
2017-04-03 13:39:55 +0800

    x

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -3,17 +3,12 @@ class OrderController < ApplicationController
     shop_url = "https://2d69dfd97a185d97d49cb4b85de5e76f:1cd78cc392fe8861b891a3f881b3c5d8@gels-store.myshopify.com/admin"
     ShopifyAPI::Base.site = shop_url
     shop = ShopifyAPI::Shop.current
-    a = 0
-    b = 0
-    c = 0
+
     #@orders = ShopifyAPI::Order.find(:all, :params => {limit: 5, order: "created_at DESC"})
-    @orders = []
     if params[:start] and params[:end]
       @orders = ShopifyAPI::Order.find(:all, :params => { :created_at_min => params[:start], :created_at_max => params[:end] })
-    elsif params[:fulfillment_status] or params[:financial_status] or params[:order_status]
-      params[:financial_status].each do |fis|
-        @orders << ShopifyAPI::Order.find(:all, :params => { :financial_status => fis })
-      end
+    elsif params[:financial_status]
+      @orders = ShopifyAPI::Order.find(:all, :params => { :financial_status => params[:financial_status] })
     else
       @orders = ShopifyAPI::Order.find(:all, :params => {limit: 5, order: "created_at DESC"})
     end
