76d07f2ded834c69196dc09d51d0054925382a57
univesta-angel <angelica.ucic@gmail.com>
2017-04-05 13:23:33 +0800

    x

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,38 +1,38 @@
 class OrderController < ApplicationController
-def index
+  def index
     shop_url = "https://2d69dfd97a185d97d49cb4b85de5e76f:1cd78cc392fe8861b891a3f881b3c5d8@gels-store.myshopify.com/admin"
     ShopifyAPI::Base.site = shop_url
     shop = ShopifyAPI::Shop.current
 
-    #@orders = ShopifyAPI::Order.find(:all, :params => {limit: 5, order: "created_at DESC"})   
-      if (params[:financial_status].present?) || (params[:fulfillment_status].present?) || (params[:order_status].present?) || (params[:start].present?) && (params[:end].present?)      
-        fls = params[:fulfillment_status]
-        fns = params[:financial_status]
-        os = params[:order_status]  
-        if (params.has_key?(:start) && params.has_key?(:end))
-          start_ = params[:start]
-          end_ = params[:end]
-          @orders = ShopifyAPI::Order.find(:all, :params => { :financial_status => fns , :fulfillment_status => fls, :order_status => os, :created_at_min => start_, :created_at_max => end_ })
-        else
-          @orders = ShopifyAPI::Order.find(:all, :params => { :financial_status => fns , :fulfillment_status => fls, :order_status => os })
-        end
-      else
-        page = 1
-        @orders = []
-        count = ShopifyAPI::Order.count
-        if count > 0
-          page += count.divmod(250).first
-          while page > 0
-            @orders += ShopifyAPI::Order.find(:all, :params => {limit: 250,  order: "created_at DESC", :page => page })
-            page -= 1
-          end
-        end
-      end #end of if else statement params
-      
+    @orders = ShopifyAPI::Order.find(:all, :params => {order: "created_at DESC"})   
+ 
   end
   
   def from_category
-    @orders = ShopifyAPI::Order.find(:all, :params => { :financial_status => "paid" })
+    if (params[:financial_status].present? || params[:fulfillment_status].present? || params[:order_status].present?)
+  		fs = params[:financial_status]
+  		fls = params[:fulfillment_status]
+  		os = params[:order_status]
+  		if !fs.blank? && !fls.blank? && !os.blank?
+  			@orders = ShopifyAPI::Order.where(:financial_status => fs, :fulfillment_status => fls, :status => os)
+  		elsif !fs.blank? && fls.blank? && os.blank?
+  			@orders = ShopifyAPI::Order.where(:financial_status => fs)
+  		elsif !fls.blank? && fs.blank? && os.blank?
+			@orders = ShopifyAPI::Order.where(:fulfillment_status => fls)
+		elsif !os.blank? && fs.blank? && fls.blank?
+			@orders = ShopifyAPI::Order.where(:status => os)
+		elsif !fs.blank? && !fls.blank? && os.blank?
+  			@orders = ShopifyAPI::Order.where(:financial_status => fs, :fulfillment_status => fls)
+  		elsif !fs.blank? && !os.blank? && fls.blank?
+			@orders = ShopifyAPI::Order.where(:financial_status => fs, :status => os)
+		elsif !os.blank? && !fls.blank? && fs.blank?
+			@orders = ShopifyAPI::Order.where(:status => os, :financial_status => fs)
+		elsif !fls.blank? && !os.blank? && fs.blank?
+			@orders = ShopifyAPI::Order.where(:status => os, :fulfillment_status => fs)
+		end
+	else
+		@orders = ShopifyAPI::Order.all
+  	end
     respond_to do |format|
       format.js
     end
