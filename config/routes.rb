45877c64b82034419b5e87a13ac2d893c9e537ed
univesta-angel <angelica.ucic@gmail.com>
2017-04-06 13:56:54 +0800

    x

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
 
-  get 'orders' => 'order#index', as: :orders
-  post 'products/bulkaction'
-  get 'my_products' => 'my_products#index', as: :my_products
-
   resources :products do
     post 'select_action'
   end
+  
+  post 'products/bulkaction'
+  get 'my_products' => 'my_products#index', as: :my_products
+  delete 'my_products/:id' => 'my_products#destroy', as: :destroy_product
+
+  get 'orders' => 'order#index', as: :orders
   get "/fetch_orders" => "order#from_category", as: :fetch_orders
+  put "/edit_note" => "order#edit_note", as: "edit_note"
+  
   root :to => 'products#index'
-  
   match 'create' => 'products#create', via: :post
-  
   post 'push' => 'products#push', as: :push
   post 'pushall' => 'products#push_all', as: :pushall
-  
   post 'edit_price' => 'products#edit_price', as: :edit_price
   post 'edit_cap' => 'products#edit_cap', as: :edit_cap
   post 'multiply_price' => 'products#multiply_price', as: :multiply_price
@@ -22,8 +23,6 @@ Rails.application.routes.draw do
   delete 'remove_all' => 'products#remove_all', as: :remove_all
   delete 'imports_remove' => 'products#remove_selected', as: :remove_selected
   
-  delete 'my_products/:id' => 'my_products#destroy', as: :destroy_product
-
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
