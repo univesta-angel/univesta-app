856db68b298b6451cf10a3d0dc68065f3772b4da
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-04-25 11:02:07 +0800

    commit

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -1,12 +1,12 @@
 class NotificationController < ApplicationController
   def index
-  	shop_url = "https://2d69dfd97a185d97d49cb4b85de5e76f:1cd78cc392fe8861b891a3f881b3c5d8@gels-store.myshopify.com/admin"
-	ShopifyAPI::Base.site = shop_url
-	shop = ShopifyAPI::Shop.current
+  	#shop_url = "https://2d69dfd97a185d97d49cb4b85de5e76f:1cd78cc392fe8861b891a3f881b3c5d8@gels-store.myshopify.com/admin"
+	#ShopifyAPI::Base.site = shop_url
+	#shop = ShopifyAPI::Shop.current
   	
   	@notifs = Notification.all.order(created_at: :desc)
   	
-  	markup = Markup.find(1)
+  	markup = Markup.find(2)
   	@pmu = markup.price
 
   end
