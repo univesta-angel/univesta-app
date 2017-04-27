6806baa80ba00adec445aad32e6677766ff6d945
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-04-27 11:30:56 +0800

    commit

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -13,24 +13,35 @@ class SettingsController < ApplicationController
   end
 
   def edit_markup
-	  	markup = Markup.find(2) 
-	  	markup.price = params[:pmu].to_f
-	  	markup.compare_at_price = params[:capmu].to_f
-	  	markup.operator1 = params[:p_operator]
-	  	markup.operator2 = params[:c_operator]
-	  	markup.cap_toggle = params[:c_toggle].to_s
-	  	markup.save
+    shop_url = "https://77d56bcf8d3ab9647b85d9030f2b1e1b:22c7a88a600583dea6c121fcb9eb1daf@gels-store.myshopify.com/admin"
+    ShopifyAPI::Base.site = shop_url
+    shop = ShopifyAPI::Shop.current
+  	markup = Markup.find(2) 
+  	markup.price = params[:pmu].to_f
+  	markup.compare_at_price = params[:capmu].to_f
+  	markup.operator1 = params[:p_operator]
+  	markup.operator2 = params[:c_operator]
+  	markup.cap_toggle = params[:c_toggle].to_s
+  	markup.save
 
-      products = ShopifyAPI::Product.all
-
-      variants = ShopifyAPP::Variant.all
-      variants.each do |v|
-        v.price = v.price + markup.price.to_f
-        v.compare_at_price + markup.compare_at_price.to_f
+    products = ShopifyAPI::Product.all
+    products.each do |product|
+      if !product.metafields.blank?
+        product.metafields.each do |metafield|
+          namespace = metafield.namespace
+          if namespace == "imported-item"
+            product.variants.each do |v|
+              v.price = v.price.to_f + markup.price
+              v.compare_at_price = v.compare_at_price.to_f + markup.compare_at_price
+            end
+          end
+        end
       end
-      variants.save
+    end
 
-  		render js: 'window.location.reload'
+    products.save
+
+		render js: 'window.location.reload'
   end
 
   def markup
