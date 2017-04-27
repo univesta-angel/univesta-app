6721c07273d712a6aac362ec418e72ae97a4d9b4
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-04-27 13:42:37 +0800

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -185,6 +185,14 @@ class ProductsController < ApplicationController
     _prc = params[:_prc2]
     _cap = params[:_compAtPrice]
     _qty = params[:_qty]
+
+    markup = Markup.find(2)
+    price_markup = markup.price
+
+    old_cost = []
+    _prc.each do |price|
+      old_cost << price.to_f - price_markup
+    end
     
     variants = []
     variantsid.each do |variant|
@@ -208,7 +216,7 @@ class ProductsController < ApplicationController
     new_product.tags = params[:_tags]
     new_product.options = options
     new_product.variants = variants
-    new_product.metafields = [{:key => params[:mf_key],:value => params[:ae_url],:value_type => "string",:namespace => "imported-item" }]
+    new_product.metafields = [{:key => params[:mf_key],:value => params[:ae_url],:value_type => "string",:namespace => "imported-item",:description => "#{old_cost}" }]
     
     pao = []
     color2 =[]     #tempo storage
