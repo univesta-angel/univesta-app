6a45662c2deb54fecaea979b4f0da5340ee4bf0a
Angelica F. Santiago <angelica.ucic@gmail.com>
2017-05-02 14:29:50 +0800

    commit

--- app/controllers/my_products_controller.rb
+++ app/controllers/my_products_controller.rb
@@ -1,8 +1,12 @@
 class MyProductsController < ApplicationController
   before_action :set_my_product, only: [:destroy]
   def index
+    shop_url = "https://77d56bcf8d3ab9647b85d9030f2b1e1b:22c7a88a600583dea6c121fcb9eb1daf@gels-store.myshopify.com/admin"
+    ShopifyAPI::Base.site = shop_url
+    shop = ShopifyAPI::Shop.current
     products = ShopifyAPI::Product.find(:all)
     @myproducts = []
+    m_namespace = ''
     products.each do |product|
       product.metafields.each do |metafield|
         m_namespace = metafield.namespace
